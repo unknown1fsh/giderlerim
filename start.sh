318995e519789735810a84355fd47f29faa18876
unknown1fsh <slmsrcncnr@gmail.com>
2026-03-29 19:58:15 +0300

    +

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Backend (Spring Boot) - port 8080
-java -jar /app/backend/app.jar --server.port=8080 &
+java -jar /app/backend/app.jar --server.port=8080 --server.address=127.0.0.1 &
 
 # Frontend (Next.js) - port 3000
 cd /app/frontend
