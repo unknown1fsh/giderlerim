da3f3fdb8dab186bda465e8b2458295474ecd03e
unknown1fsh <slmsrcncnr@gmail.com>
2026-03-29 19:41:29 +0300

    update

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ java -jar /app/backend/app.jar --server.port=8080 &
 
 # Frontend (Next.js) - port 3000
 cd /app/frontend
-PORT=3000 HOSTNAME=0.0.0.0 node server.js
+PORT=${PORT:-3000} HOSTNAME=0.0.0.0 node server.js
